f507d0c00d46d53c6f19b556aee0e562fdcba390
Gwenael Fillon <gwenael.fillon@safrangroup.com>
2021-07-30 19:18:14 +0200

    upgrade version

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -15,7 +15,7 @@ export const environment = {
   production: true,
   appTitle: 'BIADemo',
   companyName: 'TheBIADevCompany',
-  version: '1.4.0',
+  version: '1.4.1',
   logging: {
     conf: {
       serverLoggingUrl: '../WebApi/api/logs',
--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -15,7 +15,7 @@ export const environment = {
   production: false,
   appTitle: 'BIADemo',
   companyName: 'TheBIADevCompany',
-  version: '1.4.0',
+  version: '1.4.1',
   logging: {
     conf: {
       serverLoggingUrl: 'http://localhost/BIADemo/WebApi/api/logs',
