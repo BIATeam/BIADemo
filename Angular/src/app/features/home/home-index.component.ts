003d13c884f598024ace0446ef02f10da4f2f3a1
Gwenael Fillon <Gwenael.fillon@free.fr>
2022-01-23 10:12:23 +0100

    hide the breadCrump at home page

--- Angular/src/app/features/home/home-index.component.ts
+++ Angular/src/app/features/home/home-index.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BiaClassicLayoutService } from 'src/app/shared/bia-shared/components/layout/classic-layout/bia-classic-layout.service';
 
 @Component({
   selector: 'app-home-index',
   templateUrl: './home-index.component.html',
   styleUrls: ['./home-index.component.scss']
 })
-export class HomeIndexComponent {}
+export class HomeIndexComponent implements OnInit, OnDestroy{
+  constructor ( private layoutService: BiaClassicLayoutService)
+  {
+    
+  }
+  ngOnInit(): void {
+    this.layoutService.hideBreadcrumb();
+  }
+  ngOnDestroy(): void {
+    this.layoutService.showBreadcrumb();
+  }
+}
