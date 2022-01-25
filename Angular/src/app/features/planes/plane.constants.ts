f87bcebff7c6cf6d081e292408fc31e9951b1446
Gwenael Fillon <Gwenael.fillon@free.fr>
2022-01-25 17:35:05 +0100

    plurial

--- Angular/src/app/features/planes/plane.constants.ts
+++ Angular/src/app/features/planes/plane.constants.ts
@@ -1,5 +1,5 @@
 // IMPORTANT: this key should be unique in all the application. Use the path of the feature.
-export const storeKey: string = 'features-planes';
+export const storeKey: string = 'feature-planes';
 
 export const useCalcMode = false;
 export const useSignalR = false;
