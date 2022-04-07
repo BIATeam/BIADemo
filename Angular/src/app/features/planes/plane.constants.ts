f11f12277f54b8077386ea89dde12830d75e288d
Gwenael Fillon <Gwenael.fillon@free.fr>
2022-04-07 02:37:09 +0200

    add view in plane

--- Angular/src/app/features/planes/plane.constants.ts
+++ Angular/src/app/features/planes/plane.constants.ts
@@ -1,7 +1,10 @@
+import { TeamTypeId } from "src/app/shared/constants";
+
 // IMPORTANT: this key should be unique in all the application. Use the path of the feature.
 export const storeKey: string = 'feature-planes';
 
 export const useCalcMode = false;
 export const useSignalR = false;
 export const useView = false;
+export const useViewTeamId = TeamTypeId.Site;
 export const usePopup = true;
--- Angular/src/app/features/planes/views/planes-index/planes-index.component.ts
+++ Angular/src/app/features/planes/views/planes-index/planes-index.component.ts
@@ -27,7 +27,7 @@ import { loadAllView } from 'src/app/shared/bia-shared/features/view/store/views
 import { PlaneOptionsService } from '../../services/plane-options.service';
 import { PagingFilterFormatDto } from 'src/app/shared/bia-shared/model/paging-filter-format';
 import { PlaneTableComponent } from 'src/app/features/planes/components/plane-table/plane-table.component';
-import { useCalcMode, useSignalR, useView } from '../../plane.constants';
+import { useCalcMode, useSignalR, useView, useViewTeamId } from '../../plane.constants';
 import { skip } from 'rxjs/operators';
 
 @Component({
@@ -70,6 +70,7 @@ export class PlanesIndexComponent implements OnInit, OnDestroy {
   viewPreference: string;
   popupTitle: string;
   tableStateKey = this.useView ? 'planesGrid' : undefined;
+  useViewTeamId = this.useView ? useViewTeamId : null;
   parentIds: string[];
 
   constructor(
