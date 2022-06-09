ec0dd4dae642901f7d81caa5414f4679476bfdb5
Pierre-Franck CHAUVET <pierre-franck.chauvet@safrangroup.com>
2022-06-09 15:16:41 +0200

    Initial Commit of BIA.Net.Queue features

---
+++ BIAPackage/BIA.Net.Queue.Domain/RepoContract/IFileQueueRepository.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="IFileQueueRepository.cs" company="BIA">
+//  Copyright (c) BIA.Net. All rights reserved.
+// </copyright>
+
+namespace BIA.Net.Queue.Domain.RepoContract
+{
+    using System;
+    using System.Collections.Generic;
+    using BIA.Net.Queue.Domain.Dto.FileQueue;
+    using BIA.Net.Queue.Domain.Dto.Queue;
+
+    /// <summary>
+    /// Interface of FileQueueRepository
+    /// </summary>
+    public interface IFileQueueRepository
+    {
+        /// <summary>
+        /// Configure the queues.
+        /// </summary>
+        /// <param name="queues">The list of <see cref="QueueDto"/>.</param>
+        void Configure(IEnumerable<QueueDto> queues);
+
+        /// <summary>
+        /// Subscribe to recieve <see cref="FileQueueDto"/>.
+        /// </summary>
+        /// <param name="observer">The <see cref="IObserver{T}"/> of <see cref="FileQueueDto"/>.</param>
+        /// <returns></returns>
+        IDisposable Subscribe(IObserver<FileQueueDto> observer);
+
+
+        /// <summary>
+        /// Send a new <see cref="FileQueueDto"/>.
+        /// </summary>
+        /// <param name="endpoint">The endpoint.</param>
+        /// <param name="queueName">The queue name.</param>
+        /// <param name="file"> The file.</param>
+        /// <returns>A <see cref="bool"/>.</returns>
+        bool SendFile(string endpoint, string queueName, FileQueueDto file);
+    }
+}
