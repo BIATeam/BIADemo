3e8a93447d0dd8b3d64bdf1b53c76bd7efdd79c6
Gwenael Fillon <Gwenael.fillon@free.fr>
2022-06-21 17:47:15 +0200

    change version in constant

--- DotNet/TheBIADevCompany.BIADemo.Crosscutting.Common/Constants.cs
+++ DotNet/TheBIADevCompany.BIADemo.Crosscutting.Common/Constants.cs
@@ -27,7 +27,7 @@ namespace TheBIADevCompany.BIADemo.Crosscutting.Common
             /// <summary>
             /// The framework version.
             /// </summary>
-            public const string FrameworkVersion = "3.6.1";
+            public const string FrameworkVersion = "3.7.0";
         }
 
         /// <summary>
