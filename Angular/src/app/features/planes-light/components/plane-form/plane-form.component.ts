759f44fea0dfbca0d08982a386402e51f6855720
Gwenael Fillon <Gwenael.fillon@free.fr>
2022-08-25 15:46:51 +0200

    remove genericable html

--- Angular/src/app/features/planes-light/components/plane-form/plane-form.component.ts
+++ Angular/src/app/features/planes-light/components/plane-form/plane-form.component.ts
@@ -1,5 +1,4 @@
 import {
-  ChangeDetectionStrategy,
   Component,
 } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
@@ -8,9 +7,8 @@ import { Plane } from '../../model/plane';
 
 @Component({
   selector: 'app-plane-form',
-  templateUrl: './plane-form.component.html',
-  styleUrls: ['./plane-form.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default
+  templateUrl: '../../../../shared/bia-shared/feature-templates/crud-items/components/crud-item-form/crud-item-form.component.html',
+  styleUrls: ['../../../../shared/bia-shared/feature-templates/crud-items/components/crud-item-form/crud-item-form.component.scss'],
 })
 
 export class PlaneFormComponent extends CrudItemFormComponent<Plane> {
