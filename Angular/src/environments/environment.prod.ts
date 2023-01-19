54c7de010a769d476b30fc21cce7aeddf0876521
Gwenael Fillon <Gwenael.fillon@free.fr>
2023-01-19 17:35:35 +0100

    Correct ChildTeam inherit of parent team

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -3,14 +3,14 @@ import { NgxLoggerLevel } from 'ngx-logger';
 export const environment = {
   helpUrl: '',
   reportUrl: '',
-  apiUrl: '../WebApi/api',
-  hubUrl: '../WebApi/HubForClients',
+  apiUrlDynamic: ['https://[FQDN]/WebApi/api','biademo','biademo-api'],
+  hubUrlDynamic: ['https://[FQDN]/WebApi/HubForClients','biademo','biademo-api'],
   urlErrorPage: '/static/error.htm',
   useXhrWithCred: false,
   production: true,
   logging: {
     conf: {
-      serverLoggingUrl: '../WebApi/api/logs',
+      serverLoggingUrlDynamic: ['https://[FQDN]/WebApi/api/logs','biademo','biademo-api'],
       level: NgxLoggerLevel.DEBUG,
       serverLogLevel: NgxLoggerLevel.ERROR
     }
