91da504c0d3d22261309d182c1a8d64e60bb188a
Gwenael Fillon <Gwenael.fillon@free.fr>
2023-02-07 11:48:50 +0100

    add missing file

---
+++ Angular/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+import { NgxLoggerLevel } from 'ngx-logger';
+
+export const environment = {
+    helpUrl: '',
+    reportUrl: '',
+    apiUrl: '../WebApi/api',
+    hubUrl: '../WebApi/HubForClients',
+    urlErrorPage: '/static/error.htm',
+    useXhrWithCred: false,
+    production: true,
+    logging: {
+        conf: {
+            serverLoggingUrl: '../WebApi/api/logs',
+            level: NgxLoggerLevel.DEBUG,
+            serverLogLevel: NgxLoggerLevel.ERROR
+        }
+    }
+};
