05b4723ec060ee6f66c895d33c0a9e6ff73e94a4
Nicolas Mailhol <nicolas.mailhol.ext@safrangroup.com>
2023-06-16 11:03:59 +0200

    Migration BDD model

--- DotNet/TheBIADevCompany.BIADemo.Infrastructure.Data/Migrations/20230616084135_V3.7.4.Designer.cs
+++ DotNet/TheBIADevCompany.BIADemo.Infrastructure.Data/Migrations/20230616084135_V3.7.4.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheBIADevCompany.BIADemo.Infrastructure.Data;
 
@@ -11,9 +12,10 @@ using TheBIADevCompany.BIADemo.Infrastructure.Data;
 namespace TheBIADevCompany.BIADemo.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230616084135_V3.7.4")]
+    partial class V374
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DotNet/TheBIADevCompany.BIADemo.Infrastructure.Data/Migrations/20230616084135_V3.7.4.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBIADevCompany.BIADemo.Infrastructure.Data.Migrations
+{
+    public partial class V374 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Login_Domain",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Domain",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "--",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldDefaultValue: "--");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Roles",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                table: "Users",
+                column: "Login",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Login",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Domain",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "--",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "--");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Roles",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login_Domain",
+                table: "Users",
+                columns: new[] { "Login", "Domain" },
+                unique: true);
+        }
+    }
+}
