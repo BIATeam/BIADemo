b4fe91b6ee55c84a6fcaad5d53d0b82f4c7728d9
JeremieFR <jeremie.souques@outlook.fr>
2023-09-19 17:40:53 +0200

    update framework version

--- Angular/src/app/shared/bia-shared/framework-version.ts
+++ Angular/src/app/shared/bia-shared/framework-version.ts
@@ -1,1 +1,1 @@
-export const FRAMEWORK_VERSION = '3.5.5';
+export const FRAMEWORK_VERSION = '3.5.5.1';
--- DotNet/TheBIADevCompany.BIADemo.Crosscutting.Common/Constants.cs
+++ DotNet/TheBIADevCompany.BIADemo.Crosscutting.Common/Constants.cs
@@ -27,7 +27,7 @@ namespace TheBIADevCompany.BIADemo.Crosscutting.Common
             /// <summary>
             /// The framework version.
             /// </summary>
-            public const string FrameworkVersion = "3.5.5";
+            public const string FrameworkVersion = "3.5.5.1";
         }
 
         /// <summary>
