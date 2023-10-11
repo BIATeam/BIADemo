af6b425959b3f745f47e3fe7da374e3e47dd07c1
JeremieFR <36166642+JeremieFR@users.noreply.github.com>
2023-10-11 15:55:00 +0200

    update npm

--- Angular/src/app/store/state.ts
+++ Angular/src/app/store/state.ts
@@ -1,7 +1,6 @@
 import { Action, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { InjectionToken } from '@angular/core';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface AppState {
   // more state here
 }
