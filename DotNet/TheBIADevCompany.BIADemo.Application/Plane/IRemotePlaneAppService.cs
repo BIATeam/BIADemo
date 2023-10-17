4dfee4fe6aae90b000015e96bcf714c08a36f5dc
JeremieFR <jeremie.souques@outlook.fr>
2023-10-17 10:44:15 +0200

    Add example call api with RemotePlaneRepository

---
+++ DotNet/TheBIADevCompany.BIADemo.Application/Plane/IRemotePlaneAppService.cs
@@ -0,0 +1,21 @@
+﻿// BIADemo only
+// <copyright file="IRemotePlaneAppService.cs" company="TheBIADevCompany">
+// Copyright (c) TheBIADevCompany. All rights reserved.
+// </copyright>
+
+namespace TheBIADevCompany.BIADemo.Application.Plane
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface RemotePlane App Service.
+    /// </summary>
+    public interface IRemotePlaneAppService
+    {
+        /// <summary>
+        /// Examples the call API asynchronous.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task ExampleCallApiAsync();
+    }
+}
--- DotNet/TheBIADevCompany.BIADemo.Crosscutting.Ioc/IocContainer.cs
+++ DotNet/TheBIADevCompany.BIADemo.Crosscutting.Ioc/IocContainer.cs
@@ -101,6 +101,7 @@ namespace TheBIADevCompany.BIADemo.Crosscutting.Ioc
             collection.AddTransient<IPlaneTypeAppService, PlaneTypeAppService>();
             collection.AddTransient<IAirportAppService, AirportAppService>();
             collection.AddTransient<IBiaDemoTestHangfireService, BiaDemoTestHangfireService>();
+            collection.AddTransient<IRemotePlaneAppService, RemotePlaneAppService>();
 
             // End BIADemo
         }
@@ -167,6 +168,11 @@ namespace TheBIADevCompany.BIADemo.Crosscutting.Ioc
             collection.AddHttpClient<IUserProfileRepository, UserProfileRepository>().ConfigurePrimaryHttpMessageHandler(() => CreateHttpClientHandler(biaNetSection));
 
             collection.AddHttpClient<IIdentityProviderRepository, IdentityProviderRepository>().ConfigurePrimaryHttpMessageHandler(() => CreateHttpClientHandler(biaNetSection, false));
+
+            // Begin BIADemo
+            collection.AddHttpClient<IRemotePlaneRepository, RemotePlaneRepository>().ConfigurePrimaryHttpMessageHandler(() => CreateHttpClientHandler(biaNetSection));
+
+            // End BIADemo
         }
 
         /// <summary>
