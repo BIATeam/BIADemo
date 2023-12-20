47f925f16ec92ed597f2558446c547a67da3ddc8
Gwenael Fillon <Gwenael.fillon@free.fr>
2023-12-20 16:27:12 +0100

    user.Display()

--- DotNet/TheBIADevCompany.BIADemo.Domain/UserModule/Aggregate/UserOptionMapper.cs
+++ DotNet/TheBIADevCompany.BIADemo.Domain/UserModule/Aggregate/UserOptionMapper.cs
@@ -20,7 +20,7 @@ namespace TheBIADevCompany.BIADemo.Domain.UserModule.Aggregate
             return entity => new OptionDto
             {
                 Id = entity.Id,
-                Display = entity.LastName + " " + entity.FirstName + " (" + entity.Login + ")",
+                Display = entity.Display(),
             };
         }
     }
