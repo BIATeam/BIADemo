ad1fc6bb0b995a0f72520f6f93541fe871783a3c
Jeremie <jeremie.souques@outlook.fr>
2024-03-21 17:44:32 +0100

    Update CrudItemBulkSaveService

--- Angular/src/app/features/planes/services/plane-bulk-save.service.ts
+++ Angular/src/app/features/planes/services/plane-bulk-save.service.ts
@@ -3,59 +3,46 @@ import { CrudItemBulkSaveService } from 'src/app/shared/bia-shared/feature-templ
 import { Plane } from '../model/plane';
 import { OptionDto } from 'src/app/shared/bia-shared/model/option-dto';
 import { PlaneService } from './plane.service';
-import { AuthService } from 'src/app/core/bia-core/services/auth.service';
-import { TeamTypeId } from 'src/app/shared/constants';
 import { TranslateService } from '@ngx-translate/core';
-
-export interface CsvPlane extends Plane {
-  planeTypeDisplay: string;
-}
 
 @Injectable({
   providedIn: 'root',
 })
-export class PlaneBulkSaveService extends CrudItemBulkSaveService<
-  Plane,
-  CsvPlane
-> {
-  constructor(
-    planeService: PlaneService,
-    translateService: TranslateService,
-    private authService: AuthService
-  ) {
+export class PlaneBulkSaveService extends CrudItemBulkSaveService<Plane> {
+  constructor(planeService: PlaneService, translateService: TranslateService) {
     super(planeService, translateService);
   }
   public planeTypeOptions: OptionDto[];
 
-  override customMapJsonToCsv(planes: Plane[]): CsvPlane[] {
+  override customMapJsonToCsv(planes: Plane[]): Plane[] {
     return planes.map(plane => {
-      return <CsvPlane>{
+      return <Plane>{
         planeTypeDisplay: plane.planeType?.display,
         ...plane,
       };
     });
   }
 
-  override customMapCsvToJson(plane: Plane, csvPlane: CsvPlane): string[] {
-    const errorMessages: string[] = [];
+  // override customMapCsvToJson(plane: Plane, plane: Plane): string[] {
+  //   const errorMessages: string[] = [];
 
-    plane.planeType =
-      this.planeTypeOptions.find(
-        x => x.display === csvPlane?.planeTypeDisplay?.trim()
-      ) ?? null;
+  //   plane.planeType =
+  //     this.planeTypeOptions.find(
+  //       x => x.display === plane?.planeTypeDisplay?.trim()
+  //     ) ?? null;
 
-    plane.siteId =
-      plane.siteId > 0
-        ? plane.siteId
-        : this.authService.getCurrentTeamId(TeamTypeId.Site);
+  //   plane.siteId =
+  //     plane.siteId > 0
+  //       ? plane.siteId
+  //       : this.authService.getCurrentTeamId(TeamTypeId.Site);
 
-    if (
-      (csvPlane?.planeTypeDisplay?.length > 0 && plane.planeType?.id > 0) !==
-      true
-    ) {
-      errorMessages.push('This type of plane does not exist.');
-    }
+  //   if (
+  //     (plane?.planeTypeDisplay?.length > 0 && plane.planeType?.id > 0) !==
+  //     true
+  //   ) {
+  //     errorMessages.push('This type of plane does not exist.');
+  //   }
 
-    return errorMessages;
-  }
+  //   return errorMessages;
+  // }
 }
