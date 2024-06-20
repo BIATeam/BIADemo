1299ce55baaddf6a1f0f6f1004ed1033b75097ee
JeremieFR <jeremie.souques@outlook.fr>
2024-06-20 11:02:15 +0200

    fix NGXLogger setWithCredentialsOptionValue

--- Angular/src/environments/environment.ocp.ts
+++ Angular/src/environments/environment.ocp.ts
@@ -16,6 +16,7 @@ export const environment = {
       serverLoggingUrl: '/api/logs',
       level: NgxLoggerLevel.DEBUG,
       serverLogLevel: NgxLoggerLevel.ERROR,
+      withCredentials: true,
     },
   },
 };
