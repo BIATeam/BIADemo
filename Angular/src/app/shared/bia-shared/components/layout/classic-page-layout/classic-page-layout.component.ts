57003b73673bbcacb23d72dce066053dbf3de755
Gwenael Fillon <Gwenael.fillon@free.fr>
2024-06-21 10:21:14 +0200

    Wrap header

--- Angular/src/app/shared/bia-shared/components/layout/classic-page-layout/classic-page-layout.component.ts
+++ Angular/src/app/shared/bia-shared/components/layout/classic-page-layout/classic-page-layout.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'bia-classic-page-layout',
   template: `
-    <div class="mat-elevation-z2 bia-page-margin" style="width:calc(96vw);margin-left: calc(2vw);">
+    <div class="mat-elevation-z2 bia-page-margin" style="calc(95vw);">
       <ng-content></ng-content>
     </div>
   `,
