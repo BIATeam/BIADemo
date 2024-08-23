38cadf93d93dbc1799d392f0022e4dc837d2821c
Iksvihii <69140108+Iksvihii@users.noreply.github.com>
2024-08-23 15:52:57 +0200

    Simplify Plane entities models and builders (#111)
    * Remove useless collections from models
    
    * Simplify Plane mode builder
    
    ---------
    
    Co-authored-by: FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>

--- DotNet/TheBIADevCompany.BIADemo.Infrastructure.Data/ModelBuilders/PlaneModelBuilder.cs
+++ DotNet/TheBIADevCompany.BIADemo.Infrastructure.Data/ModelBuilders/PlaneModelBuilder.cs
@@ -31,37 +31,26 @@ namespace TheBIADevCompany.BIADemo.Infrastructure.Data.ModelBuilders
         /// <param name="modelBuilder">The model builder.</param>
         private static void CreatePlaneModel(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Plane>().HasKey(p => p.Id);
-            modelBuilder.Entity<Plane>().Property(p => p.SiteId).IsRequired(); // relationship 1-*
-            modelBuilder.Entity<Plane>().Property(p => p.PlaneTypeId).IsRequired(false); // relationship 0..1-*
-            modelBuilder.Entity<Plane>().Property(p => p.CurrentAirportId).IsRequired(); // relationship 0..1-*
             modelBuilder.Entity<Plane>().Property(p => p.Msn).IsRequired().HasMaxLength(64);
-            modelBuilder.Entity<Plane>().Property(p => p.Manufacturer).IsRequired(false).HasMaxLength(64);
-            modelBuilder.Entity<Plane>().Property(p => p.IsActive).IsRequired();
-            modelBuilder.Entity<Plane>().Property(p => p.IsMaintenance).IsRequired(false);
-            modelBuilder.Entity<Plane>().Property(p => p.FirstFlightDate).IsRequired();
-            modelBuilder.Entity<Plane>().Property(p => p.LastFlightDate).IsRequired(false);
-            modelBuilder.Entity<Plane>().Property(p => p.DeliveryDate).IsRequired(false);
-            modelBuilder.Entity<Plane>().Property(p => p.NextMaintenanceDate).IsRequired();
-            modelBuilder.Entity<Plane>().Property(p => p.SyncTime).IsRequired(false);
-            modelBuilder.Entity<Plane>().Property(p => p.SyncFlightDataTime).IsRequired();
-            modelBuilder.Entity<Plane>().Property(p => p.Capacity).IsRequired();
-            modelBuilder.Entity<Plane>().Property(p => p.MotorsCount).IsRequired(false);
-            modelBuilder.Entity<Plane>().Property(p => p.TotalFlightHours).IsRequired();
-            modelBuilder.Entity<Plane>().Property(p => p.Probability).IsRequired(false);
-            modelBuilder.Entity<Plane>().Property(p => p.FuelCapacity).IsRequired();
-            modelBuilder.Entity<Plane>().Property(p => p.FuelLevel).IsRequired(false);
-            modelBuilder.Entity<Plane>().Property(p => p.OriginalPrice).IsRequired().HasColumnType("Money");
-            modelBuilder.Entity<Plane>().Property(p => p.EstimatedPrice).IsRequired(false).HasColumnType("Money");
-            modelBuilder.Entity<Plane>().HasOne(x => x.CurrentAirport).WithMany().HasForeignKey(x => x.CurrentAirportId).OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<Plane>().HasOne(x => x.PlaneType).WithMany().HasForeignKey(x => x.PlaneTypeId);
+            modelBuilder.Entity<Plane>().Property(p => p.Manufacturer).HasMaxLength(64);
+            modelBuilder.Entity<Plane>().Property(p => p.OriginalPrice).HasColumnType("Money");
+            modelBuilder.Entity<Plane>().Property(p => p.EstimatedPrice).HasColumnType("Money");
+            modelBuilder.Entity<Plane>()
+                .HasOne(x => x.CurrentAirport)
+                .WithMany()
+                .HasForeignKey(x => x.CurrentAirportId)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Plane>()
+                .HasOne(x => x.PlaneType)
+                .WithMany()
+                .HasForeignKey(x => x.PlaneTypeId);
             modelBuilder.Entity<Plane>()
                 .HasMany(p => p.ConnectingAirports)
-                .WithMany(a => a.ClientPlanes)
+                .WithMany()
                 .UsingEntity<PlaneAirport>();
             modelBuilder.Entity<Plane>()
                .HasMany(p => p.SimilarTypes)
-               .WithMany(a => a.ClientPlanes)
+               .WithMany()
                .UsingEntity<PlanePlaneType>();
         }
 
@@ -71,9 +60,7 @@ namespace TheBIADevCompany.BIADemo.Infrastructure.Data.ModelBuilders
         /// <param name="modelBuilder">The model builder.</param>
         private static void CreatePlaneTypeModel(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<PlaneType>().HasKey(p => p.Id);
             modelBuilder.Entity<PlaneType>().Property(p => p.Title).IsRequired().HasMaxLength(64);
-            modelBuilder.Entity<PlaneType>().Property(p => p.CertificationDate).IsRequired(false);
         }
 
         /// <summary>
@@ -82,7 +69,6 @@ namespace TheBIADevCompany.BIADemo.Infrastructure.Data.ModelBuilders
         /// <param name="modelBuilder">The model builder.</param>
         private static void CreateAirportModel(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Airport>().HasKey(p => p.Id);
             modelBuilder.Entity<Airport>().Property(p => p.Name).IsRequired().HasMaxLength(64);
             modelBuilder.Entity<Airport>().Property(p => p.City).IsRequired().HasMaxLength(64);
         }
@@ -93,12 +79,7 @@ namespace TheBIADevCompany.BIADemo.Infrastructure.Data.ModelBuilders
         /// <param name="modelBuilder">The model builder.</param>
         private static void CreateEngineModel(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Engine>().HasKey(p => p.Id);
-            modelBuilder.Entity<Engine>().Property(p => p.PlaneId).IsRequired(); // relationship 1-*
             modelBuilder.Entity<Engine>().Property(p => p.Reference).HasMaxLength(64);
-            modelBuilder.Entity<Engine>().Property(p => p.LastMaintenanceDate).IsRequired();
-            modelBuilder.Entity<Engine>().Property(p => p.SyncTime).IsRequired();
-            modelBuilder.Entity<Engine>().Property(p => p.Power).IsRequired(false);
         }
     }
 }
