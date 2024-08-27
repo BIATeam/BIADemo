aebaf5204dedb83644672ae56fe20adf198f9dd9
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-08-27 16:09:50 +0200

    Add new keys

--- DotNet/BIAPackage/BIA.Net.Core.Common/Enum/FrontUserExceptionErrorMessageKey.cs
+++ DotNet/BIAPackage/BIA.Net.Core.Common/Enum/FrontUserExceptionErrorMessageKey.cs
@@ -32,18 +32,43 @@
         DeleteEntity,
 
         /// <summary>
-        /// Message key when errors occurs when reading entity
-        /// </summary>
-        ReadEntity,
-
-        /// <summary>
         /// Message key when errors occurs when validation failed for an entity
         /// </summary>
         ValidationEntity,
 
         /// <summary>
-        /// Message key when errors occurs due to violation of FK constraint
+        /// Message key when errors occurs in database due to violation of FK constraint
         /// </summary>
         DatabaseForeignKeyConstraint,
+
+        /// <summary>
+        /// Message key when errors occurs in database due to violation of unique constraint
+        /// </summary>
+        DatabaseUniqueConstraint,
+
+        /// <summary>
+        /// Message key when errors occurs in database due to duplicate key violation
+        /// </summary>
+        DatabaseDuplicateKey,
+
+        /// <summary>
+        /// Message key when errors occurs in database due to unable to insert NULL value
+        /// </summary>
+        DatabaseNullValueInsert,
+
+        /// <summary>
+        /// Message key when errors occurs in database due to required object not found
+        /// </summary>
+        DatabaseObjectNotFound,
+
+        /// <summary>
+        /// Message key when errors occurs in database due to login user failure
+        /// </summary>
+        DatabaseLoginUser,
+
+        /// <summary>
+        /// Message key when errors occurs in database due to opening database failure
+        /// </summary>
+        DatabaseOpen,
     }
 }
--- DotNet/BIAPackage/BIA.Net.Core.Common/Helpers/ExceptionHelper.cs
+++ DotNet/BIAPackage/BIA.Net.Core.Common/Helpers/ExceptionHelper.cs
@@ -27,6 +27,12 @@
                 FrontUserExceptionErrorMessageKey.EditEntity => "Edit {0} entity failed",
                 FrontUserExceptionErrorMessageKey.ValidationEntity => "Entity validation failed for following members : {0}\n{1}",
                 FrontUserExceptionErrorMessageKey.DatabaseForeignKeyConstraint => "An entity {0} is still linked to other entities",
+                FrontUserExceptionErrorMessageKey.DatabaseUniqueConstraint => "A similar value exists for the field {0} of entity {1}",
+                FrontUserExceptionErrorMessageKey.DatabaseDuplicateKey => "A similar value exists for the field {0} of entity {1}",
+                FrontUserExceptionErrorMessageKey.DatabaseNullValueInsert => "Field {0} cannot be NULL in entity {1}",
+                FrontUserExceptionErrorMessageKey.DatabaseObjectNotFound => "The object {0} has been not found into the database",
+                FrontUserExceptionErrorMessageKey.DatabaseLoginUser => "Unable to login to the database",
+                FrontUserExceptionErrorMessageKey.DatabaseOpen => "Unable to open the database",
                 _ => "Internal server error",
             };
         }
