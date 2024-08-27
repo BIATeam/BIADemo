4828c4ee2deca68fab62a75733d2fcc6f5d55e0b
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-08-27 15:16:51 +0200

    Adding new FrontUsrException class and associated keys

---
+++ DotNet/BIAPackage/BIA.Net.Core.Common/Enum/FrontUserExceptionErrorMessageKey.cs
@@ -0,0 +1,49 @@
+﻿namespace BIA.Net.Core.Common.Enum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Enum of availables front user exception error message keys.
+    /// </summary>
+    public enum FrontUserExceptionErrorMessageKey
+    {
+        /// <summary>
+        /// Message key when error is unkwown
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// Message key when errors occurs when adding entity
+        /// </summary>
+        AddEntity,
+
+        /// <summary>
+        /// Message key when errors occurs when editing entity
+        /// </summary>
+        EditEntity,
+
+        /// <summary>
+        /// Message key when errors occurs when deleting entity
+        /// </summary>
+        DeleteEntity,
+
+        /// <summary>
+        /// Message key when errors occurs when reading entity
+        /// </summary>
+        ReadEntity,
+
+        /// <summary>
+        /// Message key when errors occurs when validation failed for an entity
+        /// </summary>
+        ValidationEntity,
+
+        /// <summary>
+        /// Message key when errors occurs due to violation of FK constraint
+        /// </summary>
+        DatabaseForeignKeyConstraint,
+    }
+}
