1f57b30a052504b4dc469c18e0f63228d339860c
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-08-29 15:12:24 +0200

    Create Site and Airport handler services

---
+++ DotNet/TheBIADevCompany.BIADemo.WorkerService/Features/AirportHandlerRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace TheBIADevCompany.BIADemo.WorkerService.Features
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using BIA.Net.Core.Domain.RepoContract;
+    using BIA.Net.Core.Infrastructure.Data;
+    using BIA.Net.Core.WorkerService.Features.DataBaseHandler;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using TheBIADevCompany.BIADemo.Domain.PlaneModule.Aggregate;
+    using TheBIADevCompany.BIADemo.Infrastructure.Data;
+
+    public class AirportHandlerRepository : PollingDatabaseHandlerRepository<Airport, int>
+    {
+        public AirportHandlerRepository(IQueryableUnitOfWorkReadOnly dataContext)
+            : base(dataContext, OnAirportChanged)
+        {
+        }
+
+        public static void OnAirportChanged(Airport airport)
+        {
+            Debug.WriteLine($"Aiport {airport.Id} has changed");
+        }
+    }
+}
---
+++ DotNet/TheBIADevCompany.BIADemo.WorkerService/Features/SiteHandlerRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace TheBIADevCompany.BIADemo.WorkerService.Features
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using BIA.Net.Core.Infrastructure.Data;
+    using BIA.Net.Core.WorkerService.Features.DataBaseHandler;
+    using TheBIADevCompany.BIADemo.Domain.PlaneModule.Aggregate;
+    using TheBIADevCompany.BIADemo.Domain.SiteModule.Aggregate;
+
+    public class NewPlaneHandlerRepository : PollingDatabaseHandlerRepository<Plane, int>
+    {
+        public NewPlaneHandlerRepository(IQueryableUnitOfWorkReadOnly dataContext)
+            : base(dataContext, OnPlaneChanged)
+        {
+        }
+
+        public static void OnPlaneChanged(Plane plane)
+        {
+            Debug.WriteLine($"Plane {plane.Id} has changed");
+        }
+    }
+}
