1fd9c48f069fdf0e97114ce895683253fcb1e5ed
Gwenael Fillon <Gwenael.fillon@free.fr>
2024-08-30 17:34:03 +0200

    update framework version

--- DotNet/TheBIADevCompany.BIADemo.Crosscutting.Common/Constants.cs
+++ DotNet/TheBIADevCompany.BIADemo.Crosscutting.Common/Constants.cs
@@ -27,7 +27,7 @@ namespace TheBIADevCompany.BIADemo.Crosscutting.Common
             /// <summary>
             /// The framework version.
             /// </summary>
-            public const string FrameworkVersion = "3.9.0";
+            public const string FrameworkVersion = "3.10.0-alpha";
         }
 
         /// <summary>
