2b290dca836602a716e04417a50a673eb1bc94d0
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-09-23 13:38:43 +0200

    Missing BIADEmo only marker

--- DotNet/TheBIADevCompany.BIADemo.Domain/PlaneModule/Aggregate/EnginePart.cs
+++ DotNet/TheBIADevCompany.BIADemo.Domain/PlaneModule/Aggregate/EnginePart.cs
@@ -1,4 +1,5 @@
-﻿// <copyright file="EnginePart.cs" company="PlaceholderCompany">
+﻿// BIADemo only
+// <copyright file="EnginePart.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
