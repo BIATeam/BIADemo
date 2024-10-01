588498b897faff06ad59caea04688a038a225402
Loic-fr <loic.bureloux.pro@gmail.com>
2024-10-01 14:32:42 +0200

    avoid hard migration by simlifying the fix of ngrx read only and primeng lazyLoad (#159)
    Co-authored-by: Loïc BURELOUX <loic.bureloux.ext@safrangroup.com>

--- Angular/src/app/features/planes-full-code/views/planes-index/planes-index.component.ts
+++ Angular/src/app/features/planes-full-code/views/planes-index/planes-index.component.ts
@@ -25,7 +25,6 @@ import {
 import { KeyValuePair } from 'src/app/shared/bia-shared/model/key-value-pair';
 import { PagingFilterFormatDto } from 'src/app/shared/bia-shared/model/paging-filter-format';
 import { TableHelperService } from 'src/app/shared/bia-shared/services/table-helper.service';
-import { clone } from 'src/app/shared/bia-shared/utils';
 import { DEFAULT_PAGE_SIZE } from 'src/app/shared/constants';
 import { Permission } from 'src/app/shared/permission';
 import { AppState } from 'src/app/store/state';
@@ -213,14 +212,7 @@ export class PlanesIndexComponent implements OnInit, OnDestroy {
   onLoadLazy(lazyLoadEvent: TableLazyLoadEvent) {
     const pagingAndFilter: PagingFilterFormatDto = {
       parentIds: this.parentIds,
-      filters: clone(lazyLoadEvent.filters),
-      first: lazyLoadEvent.first,
-      globalFilter: clone(lazyLoadEvent.globalFilter),
-      last: lazyLoadEvent.last,
-      multiSortMeta: clone(lazyLoadEvent.multiSortMeta),
-      rows: lazyLoadEvent.rows,
-      sortField: lazyLoadEvent.sortField,
-      sortOrder: lazyLoadEvent.sortOrder,
+      ...lazyLoadEvent,
     };
     this.store.dispatch(
       FeaturePlanesActions.loadAllByPost({ event: pagingAndFilter })
