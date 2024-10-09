ddc32ecc66ab3543cd3b4884acf9c96b286c38e2
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-10-09 12:11:41 +0200

    Fix format

--- DotNet/BIAPackage/BIA.Net.Core.Presentation.Api/Filters/BiaAuthorizeFilter.cs
+++ DotNet/BIAPackage/BIA.Net.Core.Presentation.Api/Filters/BiaAuthorizeFilter.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="BiaAuthorizeFilter.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+﻿// <copyright file="BiaAuthorizeFilter.cs" company="BIA">
+//     Copyright (c) BIA. All rights reserved.
 // </copyright>
 
 namespace BIA.Net.Core.Presentation.Api.Filters
