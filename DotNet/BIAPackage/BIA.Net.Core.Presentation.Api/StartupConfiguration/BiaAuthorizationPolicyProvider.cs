1252dba69d35fbe0518030c7e154010311002286
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-10-10 14:09:50 +0200

    Format files and fix bianetconfig.json

--- DotNet/BIAPackage/BIA.Net.Core.Presentation.Api/StartupConfiguration/BiaAuthorizationPolicyProvider.cs
+++ DotNet/BIAPackage/BIA.Net.Core.Presentation.Api/StartupConfiguration/BiaAuthorizationPolicyProvider.cs
@@ -1,9 +1,9 @@
-﻿namespace BIA.Net.Core.Presentation.Api.StartupConfiguration
+﻿// <copyright file="BiaAuthorizationPolicyProvider.cs" company="BIA">
+//     Copyright (c) BIA. All rights reserved.
+// </copyright>
+
+namespace BIA.Net.Core.Presentation.Api.StartupConfiguration
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
     using BIA.Net.Core.Common.Configuration;
     using Microsoft.AspNetCore.Authentication.Negotiate;
@@ -11,17 +11,26 @@
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Options;
 
+    /// <summary>
+    /// Authorization policy provider for BIA.
+    /// </summary>
     internal class BiaAuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
     {
         private readonly BiaNetSection biaNetSection = new ();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BiaAuthorizationPolicyProvider"/> class.
+        /// </summary>
+        /// <param name="options">Authorization options</param>
+        /// <param name="configuration">Configuration</param>
         public BiaAuthorizationPolicyProvider(IOptions<AuthorizationOptions> options, IConfiguration configuration)
             : base(options)
         {
             configuration.GetSection("BiaNet").Bind(this.biaNetSection);
         }
 
-        public override async Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+        /// <inheritdoc cref="DefaultAuthorizationPolicyProvider.GetPolicyAsync(string)"/>
+        public override Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
         {
             var authenticationScheme = this.biaNetSection?.Authentication?.Keycloak?.IsActive == true ?
                 AuthenticationConfiguration.JwtBearerIdentityProvider :
@@ -32,7 +41,7 @@
                 .RequireAuthenticatedUser()
                 .Build();
 
-            return policy;
+            return Task.FromResult(policy);
         }
     }
 }
