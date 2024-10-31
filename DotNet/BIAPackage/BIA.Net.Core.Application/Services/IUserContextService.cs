6c9f7f40b62e4f7355893e515dc742e840c69f52
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-10-31 09:33:37 +0100

    Create UserContextService and use it inside MembersController to not refere to domain service layer

---
+++ DotNet/BIAPackage/BIA.Net.Core.Application/Services/IUserContextService.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IUserContextService.cs" company="BIA">
+// Copyright (c) BIA.Net. All rights reserved.
+// </copyright>
+
+namespace BIA.Net.Core.Application.Services
+{
+    /// <summary>
+    /// Interface for user context services.
+    /// </summary>
+    public interface IUserContextService
+    {
+        /// <summary>
+        /// Retrieve the user context language id.
+        /// </summary>
+        /// <returns>Language id as int.</returns>
+        int GetLanguageId();
+    }
+}
---
+++ DotNet/BIAPackage/BIA.Net.Core.Application/Services/UserContextService.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="UserContextService.cs" company="BIA">
+// Copyright (c) BIA.Net. All rights reserved.
+// </copyright>
+
+namespace BIA.Net.Core.Application.Services
+{
+    using BIA.Net.Core.Domain.Service;
+
+    /// <summary>
+    /// User context service.
+    /// </summary>
+    public class UserContextService : IUserContextService
+    {
+        private readonly UserContext userContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserContextService"/> class.
+        /// </summary>
+        /// <param name="userContext">The user context.</param>
+        public UserContextService(UserContext userContext)
+        {
+            this.userContext = userContext;
+        }
+
+        /// <inheritdoc/>
+        public int GetLanguageId()
+        {
+            return this.userContext.LanguageId;
+        }
+    }
+}
