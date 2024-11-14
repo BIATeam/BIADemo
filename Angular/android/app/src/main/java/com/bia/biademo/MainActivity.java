0bbec7bf48964d93d4e2b4b0bde6a8e36a8096f4
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-11-14 16:00:25 +0100

    Adding capacitor (android) + electron

---
+++ Angular/android/app/src/main/java/com/bia/biademo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bia.biademo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ Angular/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bia.biademo',
+  appName: 'biademo',
+  webDir: 'dist/BIADemo',
+};
+
+export default config;
