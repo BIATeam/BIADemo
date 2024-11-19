082c527ddefc168a0d0405f951a73e7983bcb8d0
Gwenael Fillon <Gwenael.fillon@free.fr>
2024-11-19 14:50:59 +0100

    add version

--- DotNet/TheBIADevCompany.BIADemo.Crosscutting.Common/Constants.cs
+++ DotNet/TheBIADevCompany.BIADemo.Crosscutting.Common/Constants.cs
@@ -27,7 +27,7 @@ namespace TheBIADevCompany.BIADemo.Crosscutting.Common
             /// <summary>
             /// The framework version.
             /// </summary>
-            public const string FrameworkVersion = "3.10.0";
+            public const string FrameworkVersion = "4.0.0-alpha";
         }
 
         /// <summary>
