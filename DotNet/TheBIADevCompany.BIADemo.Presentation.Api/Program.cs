978a9f9f95cf004b84ca3619d024ce617188e3ee
Jeremie SOUQUES <jeremie.souques@safrangroup.com>
2024-11-22 11:10:41 +0100

    Log value ASPNETCORE_ENVIRONMENT

--- DotNet/TheBIADevCompany.BIADemo.Presentation.Api/Program.cs
+++ DotNet/TheBIADevCompany.BIADemo.Presentation.Api/Program.cs
@@ -8,21 +8,26 @@ using BIA.Net.Core.Application.Authentication;
 using BIA.Net.Core.Common.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using NLog;
 using NLog.Web;
+using TheBIADevCompany.BIADemo.Crosscutting.Common;
 using TheBIADevCompany.BIADemo.Presentation.Api;
 #pragma warning restore SA1200 // Using directives should be placed correctly
+
+Logger logger = default;
 
 // NLog: setup the logger first to catch all errors
 try
 {
+    logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+    string environment = GetEnvironment(logger);
+
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
     builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-    builder.Configuration.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: false, reloadOnChange: true);
+    builder.Configuration.AddJsonFile($"appsettings.{environment}.json", optional: false, reloadOnChange: true);
     builder.Configuration.AddJsonFile("bianetconfig.json", optional: false, reloadOnChange: true);
-    builder.Configuration.AddJsonFile($"bianetconfig.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: false, reloadOnChange: true);
+    builder.Configuration.AddJsonFile($"bianetconfig.{environment}.json", optional: false, reloadOnChange: true);
 
     builder.Host.UseNLog();
 
@@ -40,13 +45,19 @@ try
 }
 catch (Exception ex)
 {
-    // NLog: catch setup errors
-    var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
-    logger.Error(ex, "Stopped program because of exception");
+    logger?.Error(ex, "Stopped program because of exception");
     throw new BIA.Net.Core.Common.Exceptions.SystemException("Stopped program because of exception", ex);
 }
 finally
 {
     // Ensure to flush and stop internal timers/threads before application-exit
     LogManager.Shutdown();
+}
+
+static string GetEnvironment(ILogger logger)
+{
+    string environment = Environment.GetEnvironmentVariable(Constants.Application.Environment);
+    string msg = $"{Constants.Application.Environment}: {environment}";
+    logger.Info(msg);
+    return environment;
 }
