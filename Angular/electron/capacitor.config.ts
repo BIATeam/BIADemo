e07ea4f6f72d940f920be557c846ba98118f10c8
Iksvihii <twigwi@gmail.com>
2024-11-26 08:35:47 +0100

    Adding 10.0.2.2 to network security of Android app + using 127.0.0.1 instead of localhost for IIS settings

--- Angular/electron/capacitor.config.ts
+++ Angular/electron/capacitor.config.ts
@@ -10,12 +10,7 @@ const config: CapacitorConfig = {
   server: {
     androidScheme: 'http',
     cleartext: true,
-  },
-  plugins: {
-    CapacitorHttp: {
-      enabled: true,
-    },
-  },
+  }
 };
 
 export default config;
