5b32752bdd036e626722c6dd122101913fa89be8
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2024-12-09 11:06:55 +0100

    Add base class for BiaDatabase and BiaRepository

---
+++ Angular/src/app/core/bia-core/bia.database.ts
@@ -0,0 +1,45 @@
+import Dexie, { Transaction } from 'dexie';
+
+export abstract class BiaDatabase extends Dexie {
+  protected constructor(databaseName: string) {
+    super(databaseName);
+  }
+
+  init() {
+    this.defineSchemas();
+  }
+
+  protected abstract defineSchemas(): void;
+
+  protected defineVersion(
+    version: number,
+    schema: any,
+    upgradeCallback?: (trans: Transaction) => Promise<void>
+  ) {
+    const versionBuilder = this.version(version).stores(schema);
+    if (upgradeCallback) {
+      versionBuilder.upgrade(async trans => {
+        console.info(`Migrate database ${this.name} to version ${version}...`);
+        try {
+          await upgradeCallback(trans);
+          console.info(
+            `Database ${this.name} has migrated successfully to version ${version}`
+          );
+        } catch (err) {
+          console.error(
+            `Fail to migrate database ${this.name} to version ${version}`,
+            err
+          );
+        }
+      });
+    }
+  }
+
+  protected deleteTable(tableName: string) {
+    const db = this as any;
+    if (db.objectStoreNames.contains(tableName)) {
+      db.deleteObjectStore(tableName);
+      console.info(`Table ${tableName} deleted on database ${this.name}`);
+    }
+  }
+}
---
+++ Angular/src/app/core/bia-core/bia.repository.ts
@@ -0,0 +1,46 @@
+import { Table } from 'dexie';
+
+export class BiaRepository<TEntity, TKey> {
+  private table: Table<TEntity, TKey>;
+
+  protected constructor(table: Table<TEntity, TKey>) {
+    this.table = table;
+  }
+
+  async create(data: TEntity): Promise<TKey> {
+    try {
+      return await this.table.add(data);
+    } catch (err) {
+      console.error(`Error inserting into table: ${this.table.name}`, err);
+      throw err;
+    }
+  }
+
+  async read(filter?: (record: TEntity) => boolean): Promise<TEntity[]> {
+    try {
+      const allRecords = await this.table.toArray();
+      return filter ? allRecords.filter(filter) : allRecords;
+    } catch (err) {
+      console.error(`Error reading from table: ${this.table.name}`, err);
+      throw err;
+    }
+  }
+
+  async update(id: TKey, changes: Partial<TEntity>): Promise<number> {
+    try {
+      return await this.table.update(id, changes);
+    } catch (err) {
+      console.error(`Error updating table: ${this.table.name}`, err);
+      throw err;
+    }
+  }
+
+  async delete(id: TKey): Promise<void> {
+    try {
+      await this.table.delete(id);
+    } catch (err) {
+      console.error(`Error deleting from table: ${this.table.name}`, err);
+      throw err;
+    }
+  }
+}
