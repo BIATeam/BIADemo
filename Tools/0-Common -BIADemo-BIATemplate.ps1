344e928821783f88dd10ca82ecfee37c38678e31
Gwenael Fillon <Gwenael.fillon@free.fr>
2024-12-18 17:25:35 +0100

    Copie .VSCode

---
+++ Tools/0-Common -BIADemo-BIATemplate.ps1
@@ -0,0 +1,30 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+Import-Module -Force $scriptPath/Library-BIA-BIATemplate.psm1
+
+# $oldName = Read-Host "old project name ?"
+$oldName = 'BIADemo'
+# $newName = Read-Host "new project name ?"
+$newName = 'BIATemplate'
+
+$jsonFileName = 'BIAToolKit.json'
+$docsFolder = '.bia'
+
+
+###### ###### ###### Process .vscode ###### ###### ######
+$newPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$scriptPath\..\..\$newName\.vscode")
+$oldPath = Resolve-Path -Path "$scriptPath\..\..\$oldName\.vscode"
+
+Write-Host "old name: " $oldName
+Write-Host "new name: " $newName
+
+
+RemoveFolderContents -path "$newPath" -Exclude ('dist', 'node_modules', '.angular')
+
+Write-Host "Copy from $oldPath to $newPath"
+Copy-Item -Path $oldPath -Destination $newPath -Recurse -Force
+
+Write-Host "replace project name"
+ReplaceProjectNameRecurse -oldName $oldName -newName $newName -Path $newPath 
+
+Write-Host "Finish"
+pause
