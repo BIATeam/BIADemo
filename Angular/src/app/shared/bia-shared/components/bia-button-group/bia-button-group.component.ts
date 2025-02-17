6ee13dd296e4104213228f78dd170ff8ef7c7de4
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-02-17 17:09:25 +0100

    Creating custom component for group of buttons

---
+++ Angular/src/app/shared/bia-shared/components/bia-button-group/bia-button-group.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, ViewChild } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'bia-button-group',
+  templateUrl: './bia-button-group.component.html',
+  styleUrl: './bia-button-group.component.scss',
+})
+export class BiaButtonGroupComponent {
+  @ViewChild('menubar') menubar: any;
+  @Input() buttons: MenuItem[];
+
+  onMenubarClick() {
+    this.setMenubarsubZIndex();
+  }
+
+  setMenubarsubZIndex() {
+    const menubarsub = this.menubar.el?.nativeElement?.querySelector(
+      'p-menubarsub'
+    ) as HTMLElement;
+    if (
+      menubarsub &&
+      menubarsub.attributes.getNamedItem('ng-reflect-mobile-active')?.value ===
+        'true'
+    ) {
+      menubarsub.style.zIndex = '998';
+    }
+  }
+}
