67f231a1455e4c5c26d9da182d0cab2312c98f50
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-02-18 10:16:58 +0100

    Always compact

--- Angular/src/app/shared/bia-shared/components/bia-button-group/bia-button-group.component.ts
+++ Angular/src/app/shared/bia-shared/components/bia-button-group/bia-button-group.component.ts
@@ -1,21 +1,38 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'bia-button-group',
   templateUrl: './bia-button-group.component.html',
   styleUrl: './bia-button-group.component.scss',
 })
-export class BiaButtonGroupComponent {
-  @ViewChild('menubar') menubar: any;
+export class BiaButtonGroupComponent implements AfterViewInit {
+  @ViewChild(Menubar) menubar: Menubar | undefined;
   @Input() buttons: ButtonGroupItem[];
+  @Input() alwaysCompact = false;
+
+  private get menubarNativeElement(): HTMLElement | undefined {
+    return this.menubar?.el?.nativeElement as HTMLElement;
+  }
+
+  ngAfterViewInit(): void {
+    if (this.alwaysCompact === true) {
+      this.compactButtons();
+    }
+  }
+
+  private compactButtons() {
+    this.menubarNativeElement?.classList.remove('bia-button-group');
+    this.menubarNativeElement?.classList.add('bia-button-group-compact');
+  }
 
   onMenubarClick() {
     this.setMenubarsubZIndex();
   }
 
   setMenubarsubZIndex() {
-    const menubarsub = this.menubar.el?.nativeElement?.querySelector(
+    const menubarsub = this.menubarNativeElement?.querySelector(
       'p-menubarsub'
     ) as HTMLElement;
     if (
