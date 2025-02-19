5910e7efb617830aa3723fddff45aa80b192ca69
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-02-19 09:59:06 +0100

    Adding method to run script from assembly embedded resources folder

---
+++ DotNet/BIAPackage/BIA.Net.Core.Common/Helpers/EmbeddedResourceHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace BIA.Net.Core.Common.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Helper for embedded resources.
+    /// </summary>
+    public static class EmbeddedResourceHelper
+    {
+        /// <summary>
+        /// Read the content of en ambedded resources.
+        /// </summary>
+        /// <param name="assembly">Assembly that contains the embedded resource.</param>
+        /// <param name="resourcePath">The embedded resource path.</param>
+        /// <returns>Content of the embedded resource as <see cref="string"/>.</returns>
+        /// <exception cref="FileNotFoundException">.</exception>
+        public static async Task<string> ReadEmbeddedResourceAsync(Assembly assembly, string resourcePath)
+        {
+            await using var stream = assembly.GetManifestResourceStream(resourcePath) ?? throw new FileNotFoundException(resourcePath);
+            using var reader = new StreamReader(stream, Encoding.UTF8);
+            return await reader.ReadToEndAsync();
+        }
+    }
+}
--- DotNet/BIAPackage/BIA.Net.Core.Infrastructure.Data/BiaDataContext.cs
+++ DotNet/BIAPackage/BIA.Net.Core.Infrastructure.Data/BiaDataContext.cs
@@ -15,6 +15,7 @@ namespace BIA.Net.Core.Infrastructure.Data
     using BIA.Net.Core.Common.Configuration;
     using BIA.Net.Core.Common.Enum;
     using BIA.Net.Core.Common.Exceptions;
+    using BIA.Net.Core.Common.Helpers;
     using BIA.Net.Core.Domain.DistCache.Entities;
     using BIA.Net.Core.Domain.Translation.Entities;
     using BIA.Net.Core.Infrastructure.Data.Helpers;
@@ -228,6 +229,36 @@ namespace BIA.Net.Core.Infrastructure.Data
         public IEntityType FindEntityType(Type entityType)
         {
             return this.Model.FindEntityType(entityType);
+        }
+
+        /// <summary>
+        /// Run SQL scripts stored in an assembly embedded resources folder.
+        /// </summary>
+        /// <param name="targetAssembly">Assembly that contains the embedded resources.</param>
+        /// <param name="relativeResourcesFolderPath">Relative path to SQL scripts folder in embedded resources.</param>
+        /// <returns><see cref="Task"/>.</returns>
+        public async Task RunScriptsFromAssemblyEmbeddedResourcesFolder(Assembly targetAssembly, string relativeResourcesFolderPath)
+        {
+            var resourcesFolderPath = string.Join('.', targetAssembly.GetName().Name, relativeResourcesFolderPath);
+            var sqlResourcesPath = targetAssembly.GetManifestResourceNames()
+                                     .Where(r => r.StartsWith(resourcesFolderPath, StringComparison.OrdinalIgnoreCase) && r.EndsWith(".sql", StringComparison.OrdinalIgnoreCase))
+                                     .ToList();
+
+            foreach (var sqlResourcePath in sqlResourcesPath)
+            {
+                var sqlScript = await EmbeddedResourceHelper.ReadEmbeddedResourceAsync(targetAssembly, sqlResourcePath);
+                if (!string.IsNullOrWhiteSpace(sqlScript))
+                {
+                    try
+                    {
+                        await this.Database.ExecuteSqlRawAsync(sqlScript);
+                    }
+                    catch (Exception ex)
+                    {
+                        this.logger.LogError(ex, "Error while executing script {ScriptPath}", sqlResourcePath);
+                    }
+                }
+            }
         }
 
         /// <summary>
