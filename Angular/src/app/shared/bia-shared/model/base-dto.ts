b8fe10812255003d0d363c016f7d7131a772ebdc
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-03-04 13:22:42 +0100

    Undefined properties base dto

--- Angular/src/app/shared/bia-shared/model/base-dto.ts
+++ Angular/src/app/shared/bia-shared/model/base-dto.ts
@@ -3,8 +3,8 @@ import { DtoState } from './dto-state.enum';
 export class BaseDto {
   id: any; // the id could be a number, a string or a GUID
   dtoState: DtoState;
-  rowVersion: string;
-  isFixed: boolean;
+  rowVersion: string | undefined;
+  isFixed: boolean | undefined;
   fixedDate: Date | undefined;
 
   constructor(id: AnalyserNode, dtoState: DtoState) {
