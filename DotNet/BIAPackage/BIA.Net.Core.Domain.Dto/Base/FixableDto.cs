0836662a89b53e00a3ac3d379c043cdbacf47093
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-03-06 09:18:38 +0100

    Add comments

--- DotNet/BIAPackage/BIA.Net.Core.Domain.Dto/Base/FixableDto.cs
+++ DotNet/BIAPackage/BIA.Net.Core.Domain.Dto/Base/FixableDto.cs
@@ -1,14 +1,25 @@
-﻿namespace BIA.Net.Core.Domain.Dto.Base
+﻿// <copyright file="FixableDto.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace BIA.Net.Core.Domain.Dto.Base
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
+    /// <summary>
+    /// Represents the DTO for fixable entity.
+    /// </summary>
+    /// <typeparam name="TKey">Key type of the DTO.</typeparam>
     public class FixableDto<TKey> : BaseDto<TKey>
     {
+        /// <summary>
+        /// Gets or sets the IsFixed.
+        /// </summary>
         public bool IsFixed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the FixedDate.
+        /// </summary>
         public DateTime? FixedDate { get; set; }
     }
 }
