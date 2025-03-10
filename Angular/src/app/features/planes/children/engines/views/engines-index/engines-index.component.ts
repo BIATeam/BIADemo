5b0bd60512e5882e9db96cb4f3d4ea7ae2953f9e
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-03-10 11:38:01 +0100

    Rename property

--- Angular/src/app/features/planes/children/engines/views/engines-index/engines-index.component.ts
+++ Angular/src/app/features/planes/children/engines/views/engines-index/engines-index.component.ts
@@ -22,7 +22,7 @@ export class EnginesIndexComponent
 {
   @ViewChild(EngineTableComponent, { static: false })
   crudItemTableComponent: EngineTableComponent;
-  isFixed = false;
+  isParentFixed = false;
 
   constructor(
     protected injector: Injector,
@@ -57,7 +57,7 @@ export class EnginesIndexComponent
       this.engineService.planeService.crudItem$
         .pipe(filter(plane => !!plane && Object.keys(plane).length > 0))
         .subscribe(plane => {
-          this.isFixed = plane.isFixed === true;
+          this.isParentFixed = plane.isFixed === true;
 
           this.canEdit =
             plane.isFixed === false &&
