849aafd845a517e97e9c0a48c003629f14fca39c
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-03-28 15:17:40 +0100

    Fix notification views issue

--- Angular/src/app/features/bia-features/notifications/views/notifications-index/notifications-index.component.ts
+++ Angular/src/app/features/bia-features/notifications/views/notifications-index/notifications-index.component.ts
@@ -9,6 +9,7 @@ import { BiaTableControllerComponent } from 'src/app/shared/bia-shared/component
 import { BiaTableHeaderComponent } from 'src/app/shared/bia-shared/components/table/bia-table-header/bia-table-header.component';
 import { BiaTableComponent } from 'src/app/shared/bia-shared/components/table/bia-table/bia-table.component';
 import { CrudItemsIndexComponent } from 'src/app/shared/bia-shared/feature-templates/crud-items/views/crud-items-index/crud-items-index.component';
+import { ViewModule } from 'src/app/shared/bia-shared/features/view/view.module';
 import { Permission } from 'src/app/shared/permission';
 import { Notification } from '../../model/notification';
 import { NotificationListItem } from '../../model/notification-list-item';
@@ -21,7 +22,6 @@ import { NotificationService } from '../../services/notification.service';
   styleUrls: ['./notifications-index.component.scss'],
   imports: [
     NgClass,
-
     PrimeTemplate,
     AsyncPipe,
     TranslateModule,
@@ -29,6 +29,7 @@ import { NotificationService } from '../../services/notification.service';
     BiaTableControllerComponent,
     BiaTableBehaviorControllerComponent,
     BiaTableComponent,
+    ViewModule,
   ],
 })
 export class NotificationsIndexComponent
