79ad6c19a64b5dec22f6dad3c62f1c0a67f9a436
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-01 11:30:04 +0200

    Fix display of engines into plane specific form

--- Angular/src/app/features/planes-specific/children/engines/components/engine-table/engine-table.component.ts
+++ Angular/src/app/features/planes-specific/children/engines/components/engine-table/engine-table.component.ts
@@ -1,3 +1,12 @@
+import {
+  AsyncPipe,
+  NgClass,
+  NgFor,
+  NgIf,
+  NgStyle,
+  NgSwitch,
+  NgTemplateOutlet,
+} from '@angular/common';
 import { Component } from '@angular/core';
 import {
   FormsModule,
@@ -5,20 +14,46 @@ import {
   UntypedFormBuilder,
 } from '@angular/forms';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { PrimeTemplate } from 'primeng/api';
+import { Skeleton } from 'primeng/skeleton';
 import { TableModule } from 'primeng/table';
+import { Tooltip } from 'primeng/tooltip';
 import { AuthService } from 'src/app/core/bia-core/services/auth.service';
 import { BiaMessageService } from 'src/app/core/bia-core/services/bia-message.service';
 import { Engine } from 'src/app/features/planes-specific/model/engine';
+import { BiaTableFilterComponent } from 'src/app/shared/bia-shared/components/table/bia-table-filter/bia-table-filter.component';
+import { BiaTableFooterControllerComponent } from 'src/app/shared/bia-shared/components/table/bia-table-footer-controller/bia-table-footer-controller.component';
+import { BiaTableInputComponent } from 'src/app/shared/bia-shared/components/table/bia-table-input/bia-table-input.component';
+import { BiaTableOutputComponent } from 'src/app/shared/bia-shared/components/table/bia-table-output/bia-table-output.component';
 import { CrudItemTableComponent } from 'src/app/shared/bia-shared/feature-templates/crud-items/components/crud-item-table/crud-item-table.component';
 
 @Component({
   selector: 'app-engine-specific-table',
   templateUrl:
-    '/src/app/shared/bia-shared/components/table/bia-calc-table/bia-calc-table.component.html',
+    '../../../../../../shared/bia-shared/components/table/bia-calc-table/bia-calc-table.component.html',
   styleUrls: [
-    '/src/app/shared/bia-shared/components/table/bia-calc-table/bia-calc-table.component.scss',
+    '../../../../../../shared/bia-shared/components/table/bia-calc-table/bia-calc-table.component.scss',
   ],
-  imports: [FormsModule, ReactiveFormsModule, TableModule, TranslateModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    TableModule,
+    PrimeTemplate,
+    NgFor,
+    Tooltip,
+    NgSwitch,
+    BiaTableFilterComponent,
+    NgClass,
+    BiaTableInputComponent,
+    NgTemplateOutlet,
+    BiaTableOutputComponent,
+    Skeleton,
+    NgStyle,
+    BiaTableFooterControllerComponent,
+    AsyncPipe,
+    TranslateModule,
+  ],
 })
 export class EngineTableComponent extends CrudItemTableComponent<Engine> {
   constructor(
