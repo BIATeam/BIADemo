8deca50d9d370889d19441f1e6cb06539663ee61
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-04-25 16:01:37 +0200

    Add automatically user as member of child team if not but member of parent team

--- DotNet/TheBIADevCompany.BIADemo.Application/User/MemberAppService.cs
+++ DotNet/TheBIADevCompany.BIADemo.Application/User/MemberAppService.cs
@@ -23,6 +23,7 @@ namespace TheBIADevCompany.BIADemo.Application.User
     using BIA.Net.Core.Domain.Specification;
     using TheBIADevCompany.BIADemo.Crosscutting.Common.Enum;
     using TheBIADevCompany.BIADemo.Domain.Dto.User;
+    using TheBIADevCompany.BIADemo.Domain.User;
     using TheBIADevCompany.BIADemo.Domain.User.Entities;
     using TheBIADevCompany.BIADemo.Domain.User.Mappers;
     using TheBIADevCompany.BIADemo.Domain.User.Specifications;
@@ -38,15 +39,21 @@ namespace TheBIADevCompany.BIADemo.Application.User
         private readonly BiaClaimsPrincipal principal;
 
         /// <summary>
+        /// The <see cref="Team"/> repository.
+        /// </summary>
+        private readonly ITGenericRepository<Team, int> teamRepository;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="MemberAppService"/> class.
         /// </summary>
         /// <param name="repository">The repository.</param>
         /// <param name="principal">The claims principal.</param>
         /// <param name="userContext">The user context.</param>
-        public MemberAppService(ITGenericRepository<Member, int> repository, IPrincipal principal)
+        public MemberAppService(ITGenericRepository<Member, int> repository, IPrincipal principal, ITGenericRepository<Team, int> teamRepository)
             : base(repository)
         {
             this.principal = principal as BiaClaimsPrincipal;
+            this.teamRepository = teamRepository;
         }
 
         /// <inheritdoc cref="IMemberAppService.GetRangeByTeamAsync"/>
@@ -97,6 +104,20 @@ namespace TheBIADevCompany.BIADemo.Application.User
             if (userId > 0 && teamId > 0)
             {
                 IList<Member> members = (await this.Repository.GetAllEntityAsync(filter: x => x.UserId == userId && x.Team.TeamTypeId == teamTypeId)).ToList();
+                var teamConfig = TeamConfig.Config.Single(t => t.TeamTypeId == teamTypeId);
+                if (!teamConfig.Parents?.IsEmpty == true)
+                {
+                    var parentTeamIds = teamConfig.Parents.Select(p => p.TeamTypeId).ToList();
+                    var parentTeams = await this.teamRepository.GetAllEntityAsync(
+                        filter: t => parentTeamIds.Any(pti => pti == t.TeamTypeId),
+                        isReadOnlyMode: true,
+                        includes: [t => t.Members]);
+
+                    if (parentTeams.SelectMany(t => t.Members).Any(m => m.UserId == userId) && !members.Any(m => m.TeamId == teamId && m.UserId == userId))
+                    {
+                        this.Repository.Add(new Member { TeamId = teamId, UserId = userId, IsDefault = true });
+                    }
+                }
 
                 if (members.Any())
                 {
@@ -104,9 +125,9 @@ namespace TheBIADevCompany.BIADemo.Application.User
                     {
                         member.IsDefault = member.TeamId == teamId;
                     }
-
-                    await this.Repository.UnitOfWork.CommitAsync();
                 }
+
+                await this.Repository.UnitOfWork.CommitAsync();
             }
         }
 
