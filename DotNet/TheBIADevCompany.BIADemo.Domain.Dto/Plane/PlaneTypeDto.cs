d1884e025a832f59630579f693d974890d520cb5
Gwenael Fillon <Gwenael.fillon@free.fr>
2025-04-30 17:19:09 +0200

    Exacxt generation for PlaneTypeDto.cs

--- DotNet/TheBIADevCompany.BIADemo.Domain.Dto/Plane/PlaneTypeDto.cs
+++ DotNet/TheBIADevCompany.BIADemo.Domain.Dto/Plane/PlaneTypeDto.cs
@@ -10,20 +10,20 @@ namespace TheBIADevCompany.BIADemo.Domain.Dto.Plane
     using BIA.Net.Core.Domain.Dto.CustomAttribute;
 
     /// <summary>
-    /// The DTO used to represent a plane.
+    /// The DTO used to represent a PlaneType.
     /// </summary>
     public class PlaneTypeDto : BaseDto<int>
     {
         /// <summary>
-        /// Gets or sets the Manufacturer's Serial Number.
+        /// Gets or sets the Title.
         /// </summary>
         [BiaDtoField(Required = true)]
         public string Title { get; set; }
 
         /// <summary>
-        /// Gets or sets the first flight date.
+        /// Gets or sets the Certification Date.
         /// </summary>
-        [BiaDtoField(Type = "datetime", Required = false)]
+        [BiaDtoField(Required = false, Type = "datetime")]
         public DateTime? CertificationDate { get; set; }
     }
 }
