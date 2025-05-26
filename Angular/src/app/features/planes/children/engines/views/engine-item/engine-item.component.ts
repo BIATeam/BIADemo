4b470ce82d16b58062a86c2d8ecfcfde0fc6a787
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-05-26 11:31:46 +0200

    Update engine and plane item component models

--- Angular/src/app/features/planes/children/engines/views/engine-item/engine-item.component.ts
+++ Angular/src/app/features/planes/children/engines/views/engine-item/engine-item.component.ts
@@ -2,8 +2,10 @@ import { AsyncPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SpinnerComponent } from 'src/app/shared/bia-shared/components/spinner/spinner.component';
+import { CrudItemService } from 'src/app/shared/bia-shared/feature-templates/crud-items/services/crud-item.service';
 import { CrudItemItemComponent } from 'src/app/shared/bia-shared/feature-templates/crud-items/views/crud-item-item/crud-item-item.component';
 import { Engine } from '../../model/engine';
+import { EngineService } from '../../services/engine.service';
 
 @Component({
   selector: 'app-engines-item',
@@ -13,5 +15,11 @@ import { Engine } from '../../model/engine';
     '../../../../../../shared/bia-shared/feature-templates/crud-items/views/crud-item-item/crud-item-item.component.scss',
   ],
   imports: [RouterOutlet, NgIf, AsyncPipe, SpinnerComponent],
+  providers: [
+    {
+      provide: CrudItemService,
+      useClass: EngineService,
+    },
+  ],
 })
 export class EngineItemComponent extends CrudItemItemComponent<Engine> {}
--- Angular/src/app/features/planes/views/plane-item/plane-item.component.ts
+++ Angular/src/app/features/planes/views/plane-item/plane-item.component.ts
@@ -2,8 +2,10 @@ import { AsyncPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SpinnerComponent } from 'src/app/shared/bia-shared/components/spinner/spinner.component';
+import { CrudItemService } from 'src/app/shared/bia-shared/feature-templates/crud-items/services/crud-item.service';
 import { CrudItemItemComponent } from 'src/app/shared/bia-shared/feature-templates/crud-items/views/crud-item-item/crud-item-item.component';
 import { Plane } from '../../model/plane';
+import { PlaneService } from '../../services/plane.service';
 
 @Component({
   selector: 'app-planes-item',
@@ -13,5 +15,11 @@ import { Plane } from '../../model/plane';
     '../../../../shared/bia-shared/feature-templates/crud-items/views/crud-item-item/crud-item-item.component.scss',
   ],
   imports: [RouterOutlet, NgIf, AsyncPipe, SpinnerComponent],
+  providers: [
+    {
+      provide: CrudItemService,
+      useClass: PlaneService,
+    },
+  ],
 })
 export class PlaneItemComponent extends CrudItemItemComponent<Plane> {}
