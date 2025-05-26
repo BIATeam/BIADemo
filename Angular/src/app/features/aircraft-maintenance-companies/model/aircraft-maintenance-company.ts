c358f2a8931de4dd3666eb2940701fc2d965693c
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-05-26 14:11:01 +0200

    Fix aircraft maintenance company model

--- Angular/src/app/features/aircraft-maintenance-companies/model/aircraft-maintenance-company.ts
+++ Angular/src/app/features/aircraft-maintenance-companies/model/aircraft-maintenance-company.ts
@@ -1,39 +1,50 @@
-import { BaseTeamDto } from 'src/app/shared/bia-shared/model/base-team-dto';
+import { Validators } from '@angular/forms';
+import {
+  BaseTeamDto,
+  teamFieldsConfigurationColumns,
+} from 'src/app/shared/bia-shared/model/base-team-dto';
 import {
   BiaFieldConfig,
   BiaFieldsConfig,
   PropType,
 } from 'src/app/shared/bia-shared/model/bia-field-config';
 import { BiaFormLayoutConfig } from 'src/app/shared/bia-shared/model/bia-form-layout-config';
+import { OptionDto } from 'src/app/shared/bia-shared/model/option-dto';
 
 // TODO after creation of CRUD Team AircraftMaintenanceCompany : adapt the model
-export type AircraftMaintenanceCompany = BaseTeamDto;
+export class AircraftMaintenanceCompany extends BaseTeamDto {
+  admins: OptionDto[];
+}
 
 // TODO after creation of CRUD Team AircraftMaintenanceCompany : adapt the field configuration
 export const aircraftMaintenanceCompanyFieldsConfiguration: BiaFieldsConfig<AircraftMaintenanceCompany> =
   {
     columns: [
-      Object.assign(new BiaFieldConfig('title', 'site.title'), {
-        isRequired: true,
-      }),
-      Object.assign(
-        new BiaFieldConfig('admins', 'aircraftMaintenanceCompany.admins'),
-        {
-          isEditable: false,
-          type: PropType.ManyToMany,
-        }
-      ),
-      Object.assign(
-        new BiaFieldConfig(
-          'rowVersion',
-          'aircraftMaintenanceCompany.rowVersion'
+      ...teamFieldsConfigurationColumns,
+      ...[
+        Object.assign(
+          new BiaFieldConfig('admins', 'aircraftMaintenanceCompany.admins'),
+          {
+            type: PropType.ManyToMany,
+            isRequired: true,
+            validators: [Validators.required],
+            // Begin BIAToolKit Generation Ignore
+            isEditable: false,
+            // End BIAToolKit Generation Ignore
+          }
         ),
-        {
-          isVisible: false,
-          isHideByDefault: true,
-          isVisibleInTable: false,
-        }
-      ),
+        Object.assign(
+          new BiaFieldConfig(
+            'rowVersion',
+            'aircraftMaintenanceCompany.rowVersion'
+          ),
+          {
+            isVisible: false,
+            isHideByDefault: true,
+            isVisibleInTable: false,
+          }
+        ),
+      ],
     ],
   };
 
