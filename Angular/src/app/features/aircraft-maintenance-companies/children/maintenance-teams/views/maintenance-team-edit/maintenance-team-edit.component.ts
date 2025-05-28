ee9f6211f9a04d9777c37f6e0a6fd05eca3a69c1
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-05-28 15:22:36 +0200

    Fix maintenance team edit component

--- Angular/src/app/features/aircraft-maintenance-companies/children/maintenance-teams/views/maintenance-team-edit/maintenance-team-edit.component.ts
+++ Angular/src/app/features/aircraft-maintenance-companies/children/maintenance-teams/views/maintenance-team-edit/maintenance-team-edit.component.ts
@@ -1,15 +1,15 @@
 import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, Injector, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 import { SpinnerComponent } from 'src/app/shared/bia-shared/components/spinner/spinner.component';
+import { FormReadOnlyMode } from 'src/app/shared/bia-shared/feature-templates/crud-items/model/crud-config';
 import { CrudItemEditComponent } from 'src/app/shared/bia-shared/feature-templates/crud-items/views/crud-item-edit/crud-item-edit.component';
+import { Permission } from 'src/app/shared/permission';
 import { MaintenanceTeamFormComponent } from '../../components/maintenance-team-form/maintenance-team-form.component';
 import { maintenanceTeamCRUDConfiguration } from '../../maintenance-team.constants';
 import { MaintenanceTeam } from '../../model/maintenance-team';
 import { MaintenanceTeamOptionsService } from '../../services/maintenance-team-options.service';
 import { MaintenanceTeamService } from '../../services/maintenance-team.service';
-import { Permission } from 'src/app/shared/permission';
-import { filter } from 'rxjs';
-import { FormReadOnlyMode } from 'src/app/shared/bia-shared/feature-templates/crud-items/model/crud-config';
 
 @Component({
   selector: 'app-maintenance-team-edit',
@@ -39,23 +39,24 @@ export class MaintenanceTeamEditComponent
   }
 
   protected setPermissions(): void {
-    // Always call this to unsubscribe existing permission subscription
     super.setPermissions();
 
-    // Define if user can fix
     this.canFix = this.authService.hasPermission(
       Permission.MaintenanceTeam_Fix
     );
 
-    // Add the subscription to dedicated permission subscription
     this.permissionSub.add(
       this.crudItemService.crudItem$
-        .pipe(filter(feature => !!feature && Object.keys(feature).length > 0))
-        .subscribe(feature => {
-          // Define the read only mode
+        .pipe(
+          filter(
+            maintenanceTeam =>
+              !!maintenanceTeam && Object.keys(maintenanceTeam).length > 0
+          )
+        )
+        .subscribe(maintenanceTeam => {
           this.formReadOnlyMode =
             this.crudConfiguration.isFixable === true &&
-            feature.isFixed === true
+            maintenanceTeam.isFixed === true
               ? FormReadOnlyMode.on
               : FormReadOnlyMode.off;
         })
