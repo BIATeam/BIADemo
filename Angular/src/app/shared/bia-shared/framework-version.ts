1de5187ec402c5ecc0a1ed81873f0963c5d9b3c8
Loïc BURELOUX <loic.bureloux.ext@safrangroup.com>
2025-07-03 10:53:14 +0200

    Update to version V5.0.1 to fix Microsof.Identity.client security risk

--- Angular/src/app/shared/bia-shared/framework-version.ts
+++ Angular/src/app/shared/bia-shared/framework-version.ts
@@ -1,1 +1,1 @@
-export const FRAMEWORK_VERSION = '5.0.0';
+export const FRAMEWORK_VERSION = '5.0.1';
--- Angular/src/environments/all-environments.ts
+++ Angular/src/environments/all-environments.ts
@@ -11,7 +11,7 @@ export const allEnvironments = {
   urlErrorPage: './assets/bia/html/error.html',
   // Except BIADemo version: '0.0.0',
   // Begin BIADemo
-  version: '5.0.0',
+  version: '5.0.1',
   // End BIADemo
 
   teams: [
