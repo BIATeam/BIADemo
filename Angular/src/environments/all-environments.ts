4e9b7698ee639ee9438089a130b18de7d231fc07
Loïc BURELOUX <loic.bureloux.ext@safrangroup.com>
2025-07-11 09:58:03 +0200

    Update version for worker service tests

--- Angular/src/environments/all-environments.ts
+++ Angular/src/environments/all-environments.ts
@@ -11,7 +11,7 @@ export const allEnvironments = {
   urlErrorPage: './assets/bia/html/error.html',
   // Except BIADemo version: '0.0.0',
   // Begin BIADemo
-  version: '5.0.2',
+  version: '5.0.3',
   // End BIADemo
 
   teams: [
