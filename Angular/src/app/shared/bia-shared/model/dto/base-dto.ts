87375c3527b27972cfc917c568fc8069fbbeb9ab
Loïc BURELOUX <loic.bureloux.ext@safrangroup.com>
2025-07-15 11:41:38 +0200

    Fix constructor type of BaseDto

--- Angular/src/app/shared/bia-shared/model/dto/base-dto.ts
+++ Angular/src/app/shared/bia-shared/model/dto/base-dto.ts
@@ -4,7 +4,7 @@ export class BaseDto {
   id: any; // the id could be a number, a string or a GUID
   dtoState: DtoState;
 
-  constructor(id: AnalyserNode, dtoState: DtoState) {
+  constructor(id: any, dtoState: DtoState) {
     this.id = id;
     this.dtoState = dtoState;
   }
