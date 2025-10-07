a611a5268ff99e1041049e38ddee1bf2ad25cf1b
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-10-07 08:58:10 +0200

    Improve bia form integration with historic tab

--- Angular/packages/bia-ng/shared/feature-templates/crud-items/components/crud-item-historic/crud-item-historic.component.ts
+++ Angular/packages/bia-ng/shared/feature-templates/crud-items/components/crud-item-historic/crud-item-historic.component.ts
@@ -1,10 +1,11 @@
+import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { HistoricEntryDto } from 'packages/bia-ng/models/dto/historic-entry-dto';
 import { TimelineModule } from 'primeng/timeline';
 
 @Component({
   selector: 'bia-crud-item-historic',
-  imports: [TimelineModule],
+  imports: [TimelineModule, DatePipe],
   templateUrl: './crud-item-historic.component.html',
   styleUrl: './crud-item-historic.component.scss',
 })
