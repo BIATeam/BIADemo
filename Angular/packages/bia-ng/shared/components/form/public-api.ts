6b2151bfcc789702a3741ff2b5d4714a5c364dd7
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2025-11-21 14:55:49 +0100

    Delete bia-input + bia-output components

--- Angular/packages/bia-ng/shared/components/form/public-api.ts
+++ Angular/packages/bia-ng/shared/components/form/public-api.ts
@@ -3,5 +3,3 @@ export * from './bia-field-base/bia-field-helper.service';
 export * from './bia-form-field/bia-form-field.component';
 export * from './bia-form-layout/bia-form-layout.component';
 export * from './bia-form/bia-form.component';
-export * from './bia-input/bia-input.component';
-export * from './bia-output/bia-output.component';
