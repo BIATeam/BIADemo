74ee7195f8ee19355f1b8d8046d46eae04f05668
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2026-01-29 16:07:45 +0100

    Remove piloe field configuration parameter for das service

--- Angular/src/app/features/planes/services/plane-das.service.ts
+++ Angular/src/app/features/planes/services/plane-das.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, Injector } from '@angular/core';
 import { AbstractDas } from 'packages/bia-ng/core/public-api';
-import { Plane, planeFieldsConfiguration } from '../model/plane';
+import { Plane } from '../model/plane';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlaneDas extends AbstractDas<Plane> {
   constructor(injector: Injector) {
-    super(injector, 'Planes', planeFieldsConfiguration);
+    super(injector, 'Planes');
   }
 }
