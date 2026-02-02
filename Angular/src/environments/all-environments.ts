95586d828600ca1459a6ba25ad4de9802d3efa17
Jeremie SOUQUES <jeremie.souques@safrangroup.com>
2026-02-02 17:16:14 +0100

    set version v6.0.2

--- Angular/src/environments/all-environments.ts
+++ Angular/src/environments/all-environments.ts
@@ -18,6 +18,6 @@ export const allEnvironments: AllEnvironments & { [key: string]: any } = {
   urlErrorPage: './assets/bia/html/error.html',
   // Except BIADemo version: '0.0.0',
   // Begin BIADemo
-  version: '6.0.1',
+  version: '6.0.2',
   // End BIADemo
 };
