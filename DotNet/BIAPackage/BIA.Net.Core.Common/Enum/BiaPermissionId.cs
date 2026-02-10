24e3fb7b5d494300ab14f6b1b70dfb57b0b3437b
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2026-02-10 14:32:52 +0100

    Explicit enum values for bia permission

--- DotNet/BIAPackage/BIA.Net.Core.Common/Enum/BiaPermissionId.cs
+++ DotNet/BIAPackage/BIA.Net.Core.Common/Enum/BiaPermissionId.cs
@@ -17,241 +17,221 @@ namespace BIA.Net.Core.Common.Enum
         /// <summary>
         /// Background Task Admin.
         /// </summary>
-        Background_Task_Admin,
+        Background_Task_Admin = 1001,
 
         /// <summary>
         /// Background Task Read Only.
         /// </summary>
-        Background_Task_Read_Only,
+        Background_Task_Read_Only = 1002,
 
         /// <summary>
         /// Notification Create.
         /// </summary>
-        Notification_Create,
+        Notification_Create = 1003,
 
         /// <summary>
         /// Notification List Access.
         /// </summary>
-        Notification_List_Access,
+        Notification_List_Access = 1004,
 
         /// <summary>
         /// Notification Delete.
         /// </summary>
-        Notification_Delete,
+        Notification_Delete = 1005,
 
         /// <summary>
         /// Notification Read.
         /// </summary>
-        Notification_Read,
+        Notification_Read = 1006,
 
         /// <summary>
         /// Notification Update.
         /// </summary>
-        Notification_Update,
+        Notification_Update = 1007,
 
         /// <summary>
         /// Roles List.
         /// </summary>
-        Roles_List,
+        Roles_List = 1008,
 
         /// <summary>
         /// Roles Options.
         /// </summary>
-        Roles_Options,
+        Roles_Options = 1009,
 
         /// <summary>
         /// Roles List For Current User.
         /// </summary>
-        Roles_List_For_Current_User,
+        Roles_List_For_Current_User = 1010,
 
         /// <summary>
         /// User Add.
         /// </summary>
-        User_Add,
+        User_Add = 1011,
 
         /// <summary>
         /// User Delete.
         /// </summary>
-        User_Delete,
+        User_Delete = 1012,
 
         /// <summary>
         /// User Save.
         /// </summary>
-        User_Save,
+        User_Save = 1013,
 
         /// <summary>
-        /// User Save.
+        /// User Read.
         /// </summary>
-        User_Read,
+        User_Read = 1014,
 
         /// <summary>
         /// User List.
         /// </summary>
-        User_List,
+        User_List = 1015,
 
         /// <summary>
         /// User ListAD.
         /// </summary>
-        User_ListAD,
+        User_ListAD = 1016,
 
         /// <summary>
         /// User List Access.
         /// </summary>
-        User_List_Access,
+        User_List_Access = 1017,
 
         /// <summary>
         /// User Sync.
         /// </summary>
-        User_Sync,
+        User_Sync = 1018,
 
         /// <summary>
         /// User Update Roles.
         /// </summary>
-        User_UpdateRoles,
+        User_UpdateRoles = 1019,
 
         /// <summary>
         /// User Options.
         /// </summary>
-        User_Options,
+        User_Options = 1020,
 
         /// <summary>
         /// LDAP Domains List.
         /// </summary>
-        LdapDomains_List,
+        LdapDomains_List = 1021,
 
         /// <summary>
         /// Languages Options.
         /// </summary>
-        Languages_Options,
+        Languages_Options = 1022,
 
         /// <summary>
         /// Profile Image Get.
         /// </summary>
-        ProfileImage_Get,
+        ProfileImage_Get = 1023,
 
         /// <summary>
         /// Logs Create.
         /// </summary>
-        Logs_Create,
+        Logs_Create = 1024,
 
         /// <summary>
         /// Team Access All.
         /// </summary>
-        Team_Access_All,
+        Team_Access_All = 1025,
 
         /// <summary>
         /// Team List Access.
         /// </summary>
-        Team_List_Access,
+        Team_List_Access = 1026,
 
         /// <summary>
         /// Team Options.
         /// </summary>
-        Team_Options,
+        Team_Options = 1027,
 
         /// <summary>
         /// Team Set Default Team.
         /// </summary>
-        Team_Set_Default_Team,
+        Team_Set_Default_Team = 1028,
 
         /// <summary>
         /// Team Set Default Roles.
         /// </summary>
-        Team_Set_Default_Roles,
+        Team_Set_Default_Roles = 1029,
 
         /// <summary>
         /// View List.
         /// </summary>
-        View_List,
+        View_List = 1030,
 
         /// <summary>
         /// View Read.
         /// </summary>
-        View_Read,
+        View_Read = 1031,
 
         /// <summary>
         /// View Add User View.
         /// </summary>
-        View_Add_UserView,
-
-        /// <summary>
-        /// View Add Team View Suffix.
-        /// </summary>
-        View_Add_TeamViewSuffix,
+        View_Add_UserView = 1032,
 
         /// <summary>
         /// View Update User View.
         /// </summary>
-        View_Update_UserView,
-
-        /// <summary>
-        /// View Update Team View Suffix.
-        /// </summary>
-        View_Update_TeamViewSuffix,
+        View_Update_UserView = 1033,
 
         /// <summary>
         /// View Delete User View.
         /// </summary>
-        View_Delete_UserView,
+        View_Delete_UserView = 1034,
 
         /// <summary>
         /// View Delete Team View.
         /// </summary>
-        View_Delete_TeamView,
+        View_Delete_TeamView = 1035,
 
         /// <summary>
         /// View Set Default User View.
         /// </summary>
-        View_Set_Default_UserView,
-
-        /// <summary>
-        /// View Set Default Team View Suffix.
-        /// </summary>
-        View_Set_Default_TeamViewSuffix,
-
-        /// <summary>
-        /// View Assign To Team Suffix.
-        /// </summary>
-        View_Assign_To_TeamSuffix,
+        View_Set_Default_UserView = 1036,
 
         /// <summary>
         /// Impersonation Connection Rights.
         /// </summary>
-        Impersonation_Connection_Rights,
+        Impersonation_Connection_Rights = 1037,
 
         /// <summary>
         /// Announcement Create.
         /// </summary>
-        Announcement_Create,
+        Announcement_Create = 1038,
 
         /// <summary>
         /// Announcement Delete.
         /// </summary>
-        Announcement_Delete,
+        Announcement_Delete = 1039,
 
         /// <summary>
         /// Announcement List Access.
         /// </summary>
-        Announcement_List_Access,
+        Announcement_List_Access = 1040,
 
         /// <summary>
         /// Announcement Read.
         /// </summary>
-        Announcement_Read,
+        Announcement_Read = 1041,
 
         /// <summary>
         /// Announcement Update.
         /// </summary>
-        Announcement_Update,
+        Announcement_Update = 1042,
 
         /// <summary>
         /// Notification Type Options.
         /// </summary>
-        NotificationType_Options,
+        NotificationType_Options = 1043,
 
         /// <summary>
         /// Announcement Type Options.
         /// </summary>
-        AnnouncementType_Options,
+        AnnouncementType_Options = 1044,
     }
 }
