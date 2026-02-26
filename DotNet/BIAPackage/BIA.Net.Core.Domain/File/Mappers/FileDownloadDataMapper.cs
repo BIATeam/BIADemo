20bac1b154f42a4d37334b35a6111366d8c3127f
FOLIOT Kevin <kevin.foliot.ext@safrangroup.com>
2026-02-26 12:47:13 +0100

    Complete mapper

--- DotNet/BIAPackage/BIA.Net.Core.Domain/File/Mappers/FileDownloadDataMapper.cs
+++ DotNet/BIAPackage/BIA.Net.Core.Domain/File/Mappers/FileDownloadDataMapper.cs
@@ -5,6 +5,7 @@
 namespace BIA.Net.Core.Domain.File.Mappers
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq.Expressions;
     using BIA.Net.Core.Common.Extensions;
     using BIA.Net.Core.Domain.Dto.File;
@@ -17,10 +18,24 @@ namespace BIA.Net.Core.Domain.File.Mappers
     /// </summary>
     public class FileDownloadDataMapper : BaseMapper<FileDownloadDataDto, FileDownloadData, Guid>
     {
-        /// <summary>
-        /// Create a file download data DTO from an entity.
-        /// </summary>
-        /// <returns>The file download data DTO.</returns>
+        /// <inheritdoc />
+        public override ExpressionCollection<FileDownloadData> ExpressionCollection
+        {
+            get
+            {
+                return new ExpressionCollection<FileDownloadData>(base.ExpressionCollection)
+                {
+                    { HeaderName.FileName, entity => entity.FileName },
+                    { HeaderName.FileContentType, entity => entity.FileContentType },
+                    { HeaderName.FilePath, entity => entity.FilePath },
+                    { HeaderName.RequestDateTime, entity => entity.RequestDateTime },
+                    { HeaderName.RequestByUser, entity => entity.RequestByUser.FirstName + " " + entity.RequestByUser.LastName },
+                    { HeaderName.AvailabilityDuration, entity => entity.ExpiredAtDateTime },
+                };
+            }
+        }
+
+        /// <inheritdoc />
         public override Expression<Func<FileDownloadData, FileDownloadDataDto>> EntityToDto()
         {
             return base.EntityToDto().CombineMapping(entity => new FileDownloadDataDto
@@ -38,21 +53,66 @@ namespace BIA.Net.Core.Domain.File.Mappers
             });
         }
 
-        /// <summary>
-        /// Create a file download data entity from a DTO.
-        /// </summary>
-        /// <param name="dto">The DTO.</param>
-        /// <param name="entity">The entity to update with the DTO values.</param>
+        /// <inheritdoc />
         public override void DtoToEntity(FileDownloadDataDto dto, ref FileDownloadData entity)
         {
-            entity ??= new FileDownloadData();
-            entity.Id = dto.Id;
+            base.DtoToEntity(dto, ref entity);
             entity.FileName = dto.FileName;
             entity.FileContentType = dto.FileContentType;
             entity.FilePath = dto.FilePath;
             entity.RequestDateTime = dto.RequestDateTime;
             entity.RequestByUserId = dto.RequestByUser.Id;
             entity.ExpiredAtDateTime = dto.AvailabilityDuration.HasValue ? dto.RequestDateTime.Add(dto.AvailabilityDuration.Value) : null;
+        }
+
+        /// <inheritdoc />
+        public override Dictionary<string, Func<string>> DtoToCellMapping(FileDownloadDataDto dto)
+        {
+            return new Dictionary<string, Func<string>>(base.DtoToCellMapping(dto))
+            {
+                { HeaderName.FileName, () => CSVString(dto.FileName) },
+                { HeaderName.FileContentType, () => CSVString(dto.FileContentType) },
+                { HeaderName.FilePath, () => CSVString(dto.FilePath) },
+                { HeaderName.RequestDateTime, () => CSVDateTime(dto.RequestDateTime) },
+                { HeaderName.RequestByUser, () => CSVString(dto.RequestByUser?.Display) },
+                { HeaderName.AvailabilityDuration, () => CSVTime(dto.AvailabilityDuration) },
+            };
+        }
+
+        /// <summary>
+        /// Header Name.
+        /// </summary>
+        public struct HeaderName
+        {
+            /// <summary>
+            /// Header Name FileName.
+            /// </summary>
+            public const string FileName = "fileName";
+
+            /// <summary>
+            /// Header Name FileContentType.
+            /// </summary>
+            public const string FileContentType = "fileContentType";
+
+            /// <summary>
+            /// Header Name FilePath.
+            /// </summary>
+            public const string FilePath = "filePath";
+
+            /// <summary>
+            /// Header Name RequestDateTime.
+            /// </summary>
+            public const string RequestDateTime = "requestDateTime";
+
+            /// <summary>
+            /// Header Name RequestByUser.
+            /// </summary>
+            public const string RequestByUser = "requestByUser";
+
+            /// <summary>
+            /// Header Name AvailabilityDuration.
+            /// </summary>
+            public const string AvailabilityDuration = "availabilityDuration";
         }
     }
 }
